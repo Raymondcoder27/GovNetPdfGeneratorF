d62a33d939c7eb0559147b9dbac35d02bb9bdc5d
Mwebe Raymond <mwebraymond13@gmail.com>
2024-07-29 14:56:50 +0300

    fixind tailwind

--- govNetDocGenerator/tailwind.config.js
+++ govNetDocGenerator/tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    './src/**/*.{html,js,vue}', // This ensures Tailwind scans your HTML, JS, and Vue files
+    './public/index.html', // Add this if your main HTML file is in the public directory
+ 
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
+
 
