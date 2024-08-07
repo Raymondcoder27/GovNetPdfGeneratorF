aec387cd86ee3b2eb601f7260cc2810b984d2677
Raymond Mwebe <122720713+Raymondcoder27@users.noreply.github.com>
2024-08-07 16:14:32 +0300

    Update taskStore.js

--- govNetDocGenerator/src/stores/taskStore.js
+++ govNetDocGenerator/src/stores/taskStore.js
@@ -26,36 +26,6 @@ export const useDocumentStore = defineStore('documentStore', {
       const res = await fetch('http://localhost:8000/generated-file')
       const data = await res.json()
     },
-    // async createDoc(task) {
-    //    this.tasks.push(task)
-
-    //   const res = await fetch('http://localhost:8080/generate', {
-    //   method: 'POST',
-    //   body: JSON.stringify(task),
-    //   headers: {'Content-Type':'application/json'}
-    // })
-    //   if (res.error){
-    //     console.log(res.error)
-    //   }
-    // },
-
-    // async uploadData(file, jsonData) {
-    //   const formData = new FormData();
-    //   formData.append("file", file);
-    //   formData.append("data", jsonData);
-
-    //   try {
-    //     const response = await fetch("http://localhost:8080/generate", {
-    //       method: "POST",
-    //       body: formData,
-    //     });
-
-    //     const result = await response.json();
-    //     console.log(result);
-    //   } catch (error) {
-    //     console.error("Error uploading data:", error);
-    //   }
-    // }
   },
     deleteTask(id){
       this.tasks = this.tasks.filter(t => {
